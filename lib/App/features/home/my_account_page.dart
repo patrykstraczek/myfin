3b1d3ac4b4efd670b1908e05f81ddadc6f04d64c
Patryk Strączek <patrykstraczek99@gmail.com>
2022-06-11 10:57:43 +0200

    manage imports

--- lib/App/features/home/my_account_page.dart
+++ lib/App/features/home/my_account_page.dart
@@ -1,9 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myfin/App/features/home/info_page.dart';
-import 'package:myfin/app/cubit/root_cubit.dart';
 
 class MyAccountPage extends StatelessWidget {
   const MyAccountPage({
