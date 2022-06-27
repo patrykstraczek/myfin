8caae6db19754f7dfa13601d3c095207ec3e9bef
Patryk Strączek <patrykstraczek99@gmail.com>
2022-06-27 18:21:39 +0200

    organize rest of the files

--- lib/App/app.dart
+++ lib/App/app.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MyFin - Moje Finanse',
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
