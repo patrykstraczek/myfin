cbc2c5e6af4843e1903374f9e8a3bcacac71f183
Patryk Strączek <patrykstraczek99@gmail.com>
2023-05-03 11:56:36 +0200

    Add phone number authorization

--- lib/App/features/auth/pages/sign_in_screen.dart
+++ lib/App/features/auth/pages/sign_in_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
-import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
@@ -11,7 +9,7 @@ class SignInPage extends StatelessWidget {
     return SignInScreen(
       providers: [
         EmailAuthProvider(),
-        GoogleProvider(clientId: '...')
+        PhoneAuthProvider(),
       ],
       actions: [
         AuthStateChangeAction<SignedIn>((context, state) {
